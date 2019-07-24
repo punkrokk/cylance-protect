dcc8826399b41438057bef752cfc6ad502420389
alexsch <alex@syncurity.net>
2019-07-24 00:12:37 -0400

    First commit

---
+++ actions/cylance_add_hash.py
@@ -0,0 +1,33 @@
+import json
+
+from base import CylanceBaseAction
+from st2common.runners.base_action import Action
+from cylance import Cylance
+
+
+class CylanceAddHash(Action):
+    """Add an hash value to a Quarantine List"""
+    def run(self, hash_value, list_type, category=None, reason=None):
+
+        try:
+            cylance = Cylance(tenant_value=tenant_value, app_id=app_id, app_secret=app_secret)
+        except (ValueError, KeyError) as e:
+            self.logger.error(e.__str__())
+            sys.exit(1)
+
+        response = cylance.add_hash_to_list(hash_value, list_type, category, reason)
+
+        fact_data = {}
+
+        if response:
+
+            response_dict = json.loads(response.content)
+            fact_data = {
+                'sha256': response_dict['sha256'],
+                'category': response_dict['category'],
+                'listType': response_dict['list_type'],
+                'reason': response_dict['reason'],
+                'action': "Successfully added hash to the specified list."
+            }
+
+        return True, fact_data
