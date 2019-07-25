643dd24ed5ec5faa46d91281e8a796a7a9c6d31f
alexsch <alex@syncurity.net>
2019-07-25 12:54:20 -0400

    Updated Action

--- actions/cylance_add_hash.py
+++ actions/cylance_add_hash.py
@@ -6,18 +6,10 @@ from cylance import CylanceProtectClient
 
 
 class CylanceAddHash(Action):
-    """Add a hash value to a Quarantine List"""
+    """Add a hash value to a Quarantine or Safe List"""
     def run(self, hash_value, list_type, category=None, reason=None):
 
-        try:
-            cylance = CylanceProtectClient(tenant_value=tenant_value, app_id=app_id, app_secret=app_secret)
-        except (ValueError, KeyError) as e:
-            self.logger.error(e.__str__())
-            sys.exit(1)
-
-        response = cylance.add_hash_to_list(hash_value, list_type, category, reason)
-
-        fact_data = {}
+        response = self.cylance.add_hash_to_list(hash_value, list_type, category, reason)
 
         if response:
 
@@ -30,4 +22,7 @@ class CylanceAddHash(Action):
                 'action': "Successfully added hash to the specified list."
             }
 
-        return True, fact_data
+            return True, fact_data
+        else:
+            return False, "Hash could not be added to the specified list."
+
