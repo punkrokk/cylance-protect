05d055731e8725e24707d546869e9b1aae985c02
alexsch <alex@syncurity.net>
2019-07-31 15:23:18 -0400

    Remove hash error

--- actions/cylance_remove_hash.py
+++ actions/cylance_remove_hash.py
@@ -8,14 +8,13 @@ class CylanceRemoveHash(CylanceBaseAction):
     """Remove a hash value from a Quarantine or Safe List"""
     def run(self, hash_value, list_type):
 
-        response = self.cylance.remove_hash(hash_value, list_type)
+        response = self.cylance.remove_hash_from_list(hash_value, list_type)
         if response[0]:
 
             # the response only returns success or failure
             fact_data = {
                 'action': "Successfully removed hash from the specified list."
             }
-
             return True, fact_data
         else:
             return False, "Hash could not be removed from the specified list."
