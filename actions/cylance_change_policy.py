1676a0256fb62bf0604eb91b4d0ceb3ab17b15c3
alexsch <alex@syncurity.net>
2019-07-31 17:14:48 -0400

    Cylance change policy

---
+++ actions/cylance_change_policy.py
@@ -0,0 +1,27 @@
+import json
+
+from base import CylanceBaseAction
+from st2common.runners.base_action import Action
+
+
+class CylanceChangePolicy(CylanceBaseAction):
+    """Get Threat Information given a Hash Value"""
+    def run(self, device_name, policy_name):
+
+        response = self.cylance.change_policy(device_name, policy_name)
+
+        if response[0]:
+
+            try:
+                response_dict = response[1].json()
+            except:
+                return False, 'Could not parse JSON response'
+
+            fact_data = {
+                'response': response_dict
+            }
+
+            return True, fact_data
+        else:
+            return False, "Cylance could not retrieve threat information given the provided hash value"
+
