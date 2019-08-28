cc9687bc3be56e03bd58a3bb6def097e0195e539
JP Bourget <jp@syncurity.net>
2019-08-28 15:53:57 -0400

    test response

--- actions/get_device.py
+++ actions/get_device.py
@@ -6,8 +6,13 @@ class CylanceGetDevice(CylanceBaseAction):
     def run(self, device_name):
 
         response = self.cylance.get_device(device_name)
-        if response:
+        if response[0]:
 
-            return True, response
+            try:
+                response_dict = response[1].json()
+            except:
+                return False, 'Could not parse JSON response'
+
+            return True, response_dict
         else:
             return False, "Hash could not retrieve device information given the provided name"
